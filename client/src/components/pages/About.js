dbb3fad939be6434add3c40bae20dc095f4105f6
NguyenHuuDuc <nguyenhuuduc110800@gmail.com>
2020-09-29 20:58:06 +0700

    add fr

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
             <h1>Bài Tập Về Nhà Tuần 1</h1>
             <p>Sinh Viên Thực Hiện:</p>
             <h4>A32387 - Nguyễn Hữu Đức - 0563045360</h4>
-            <h4>A32387 - Nguyễn Hữu Đức - 0563045360</h4>
+            <h4>A34489 - Nguyễn Thị Thùy Dung - 0971499810</h4>
         </div>
     )
 }
